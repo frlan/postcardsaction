b949b432ff7086d3e19d16d6cc8b4f537b4531f6
Frank Lanitz <frank@frank.uvena.de>
2020-04-13 19:28:12 +0200

    Postcrossing: Use Meta-class to customize model a little

--- postcardsaction/postcrossing/models.py
+++ postcardsaction/postcrossing/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class PCUser(models.Model):
     username = models.CharField(
-        help_text="The username of postcrossingg user",
+        help_text="The username of postcrossing user",
         unique=True,
         max_length=100,
     )
@@ -11,9 +11,15 @@ class PCUser(models.Model):
     def __str__(self):
         return self.username
 
+    class Meta:
+        ordering = ["username"]
+        verbose_name = "Postcrossing User"
+
+
 
 class PCPostCard(models.Model):
     pc_id = models.CharField(
+        "Postcrossing ID",
         help_text="The Key at poscrossing", max_length=100, unique=True
     )
     from_user = models.ForeignKey(
@@ -32,3 +38,8 @@ class PCPostCard(models.Model):
         Returns URL for a postcrossing postcard
         """
         return "https://www.postcrossing.com/postcards/{}".format(self.pc_id)
+
+    class Meta:
+        ordering = ["-pc_id"]
+        verbose_name = "Postcrossing Card"
+
