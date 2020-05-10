bb6f2cba2f0aa661603662f443f7d22f4f48efda
Frank Lanitz <frank@frank.uvena.de>
2020-05-10 10:26:56 +0200

    Copyrighter: Add small pieces of docstring to classes

--- postcardsaction/copyrighter/models.py
+++ postcardsaction/copyrighter/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class Holder(models.Model):
+    """
+    A class to collect information about the holder
+    (= who is having the copyright) of something
+    """
+
     name = models.CharField(max_length=100)
     url = models.URLField(blank=True)
 
@@ -11,7 +16,12 @@ class Holder(models.Model):
     class Meta:
         ordering = ["name"]
 
+
 class Licence(models.Model):
+    """
+    A special typ of licence. This could be for example CC-BY-SA or GPLv3
+    """
+
     slug = models.CharField(max_length=30)
     name = models.CharField(max_length=200)
     description = models.TextField(default="", blank=True)
@@ -21,6 +31,13 @@ class Licence(models.Model):
 
 
 class Copyright(models.Model):
+    """
+    A combination of a copyright holder is apply a special licence.
+
+    This is what you actually want to connect to something that is listed
+    under some copyright at all
+    """
+
     holder = models.ForeignKey(Holder, on_delete=models.CASCADE)
     licence = models.ForeignKey(
         Licence, on_delete=models.CASCADE, null=True, blank=True
