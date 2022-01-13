bbfe8738c811781ae9ddc885e05d06dd4176ea81
Frank Lanitz <frank@frank.uvena.de>
2022-01-13 18:15:33 +0100

    Enable Django 4.x

--- postcardsaction/postcardsaction/urls.py
+++ postcardsaction/postcardsaction/urls.py
@@ -1,39 +1,24 @@
-"""postcardsaction URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.conf import settings
-from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic.base import TemplateView
 from postcard.views import IndexView
 from postcard.views import PostcardDetailView
 from postcard.views import LatestPostcardsFeed
 from postcard.views import OriginatorDetailView
+from postcard.views import OriginatorIndexView
+
 
 urlpatterns = [
-    url(r"^$", IndexView.as_view(template_name="index.html"), name="index"),
-    url(r'^feed/$', LatestPostcardsFeed(), name='news_feed'),
+    re_path(r"^$", IndexView.as_view(template_name="index.html"), name="index"),
+    re_path(r'^feed/$', LatestPostcardsFeed(), name='news_feed'),
     path("admin/", admin.site.urls),
-    url(
+    re_path(
         r"^(?P<pk>[\w]+)/$", PostcardDetailView.as_view(), name="postcard_detail",
     ),
-    url(
+    re_path(
         r"^o/(?P<pk>[\w]+)/$",
             OriginatorDetailView.as_view(),
             name="originator_detail"
