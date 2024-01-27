d9ada0a6cd02d84b953a09704cb159bd672974c7
Frank Lanitz <frank@frank.uvena.de>
2024-01-27 22:31:46 +0100

    Postcard: Allow to search for an URL inside admin interface

--- postcardsaction/postcard/admin.py
+++ postcardsaction/postcard/admin.py
@@ -11,14 +11,16 @@ from .models import URL
 admin.site.register(Country)
 admin.site.register(PostcardItem)
 admin.site.register(Series)
-admin.site.register(URL)
-
 
 @admin.register(Postcard)
 class PostcardAdmin(admin.ModelAdmin):
     list_display = ["title", "id", "published", "publishing_date"]
     search_fields = ("title",)
 
+@admin.register(URL)
+class URLAdmin(admin.ModelAdmin):
+    list_display = ["title", "url"]
+    search_fields = ("title",)
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
