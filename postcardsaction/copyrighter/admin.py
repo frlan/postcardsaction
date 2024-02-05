b7e86fa90a679191b133f103eb1aeb10b5e8eea7
Frank Lanitz <frank@frank.uvena.de>
2024-02-05 20:53:05 +0100

    Copyrighter: Add search and filter options to admin backend

--- postcardsaction/copyrighter/admin.py
+++ postcardsaction/copyrighter/admin.py
@@ -3,6 +3,20 @@ from .models import Copyright
 from .models import Holder
 from .models import Licence
 
+
+class HolderAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = [
+        "name",
+    ]
+
+
+class LicenceAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    list_filter = ("name", "slug")
+    search_fields = ["name", "fields"]
+
+
 admin.site.register(Copyright)
-admin.site.register(Holder)
-admin.site.register(Licence)
+admin.site.register(Holder, HolderAdmin)
+admin.site.register(Licence, LicenceAdmin)
